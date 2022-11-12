67f4b4b350d4f13ba2041e7658f8f69632d108dc
yNegus <100074639+yNegus@users.noreply.github.com>
2022-11-12 16:42:41 +0700

    Update and rename ThemeManager.lua.txt to ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'LinoriaLibSettings'
+	ThemeManager.Folder = 'silverwrld'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
