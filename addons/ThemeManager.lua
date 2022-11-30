81909e50eb295d700b39cfbadbe3fd4fc6515187
yNegus <100074639+yNegus@users.noreply.github.com>
2022-11-30 17:41:09 +0700

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'silverwrld'
+	ThemeManager.Folder = 'silver'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
